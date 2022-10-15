c1199681f9cf02a4117010a028d9511ea7867f67
monkuma <kumamoto.jobs@gmail.com>
2022-10-15 18:11:14 +0900

    all done

---
+++ components/Layout.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilsStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "Kuma Code";
+export const siteTitle = "Next.js Blog.";
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img
+          className={utilsStyles.borderCircle}
+          src="/images/profile.png"
+          height={100}
+          width={100}
+        />
+        <h1 className={utilsStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+
+      {!home && (
+        <div>
+          <Link href="/"> ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/posts/firstPost.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+
+Link;
+export default function FirstPost() {
+  return (
+    <div>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
+      <h1>最初の投稿</h1>
+      <h2>
+        <Link href="/">ホームへ戻る</Link>
+      </h2>
+    </div>
+  );
+}
